ea40c9d4f271a39229687a39cddc8b9a7f52a997
magozheon <32003475+magozheon@users.noreply.github.com>
2017-09-19 16:52:06 -0300

    Atualização e inclusão de módulos do sistema
    Adição de configuração base e inclusão de modal com formulário de configuração.
    Correção de alguns pequenos bugs e adição de algumas melhorias.

--- js/distorcao.js
+++ js/distorcao.js
@@ -1,1 +1,16 @@
 var app = angular.module('distorcao', []);
+
+app.run(function($rootScope){
+	/*$rootScope.attNivel = 0;
+	$rootScope.attVigor = 0;
+	$rootScope.attDeterminacao = 0;
+	$rootScope.attForca = 0;
+	$rootScope.attDestreza = 0;
+	$rootScope.attMental = 0;
+	$rootScope.resultadoVida = 400;
+	$rootScope.resultadoPeso = 40;
+	$rootScope.resultadoBonusForca = 0;
+	$rootScope.resultadoBonusDestreza = 0;
+	$rootScope.resultadoBonusMental = 0;
+	$rootScope.resultadoMovimentacao = 2;*/
+});
