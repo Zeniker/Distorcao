aad2baf0be6f6267ae5275e01fe3418cfcdd63df
Guilherme Lopes Lacerda da Silva <gui.lls@hotmail.com>
2017-09-29 16:13:04 -0300

    Alterado para calcular as atividades realizadas no turno e colocar no log de combate.

--- js/controllers/simulador.controller.js
+++ js/controllers/simulador.controller.js
@@ -10,10 +10,14 @@ function simuladorController() {
     vm.removeCombatente = removeCombatente;
     vm.importaAcoes = importaAcoes;
     vm.teste = teste;
+    vm.iniciaCombate = iniciaCombate;
 
     //Variáveis
     vm.combatenteSimulador = {};
     vm.acoes = [];
+    vm.posicaoTurno = 5;
+    vm.logCombate = "";
+    vm.atividadeTurno = [];
 
     function novoCombatente() {
         if (Object.keys(vm.combatenteSimulador).length > 0) {
@@ -43,5 +47,82 @@ function simuladorController() {
     function teste() {
         console.log(vm.combatenteSimulador);
     }
+
+    function calculaPosicaoTurno(valor){
+        vm.posicaoTurno = (valor * 100) / 150;
+    }
+
+    function iniciaCombate(){        
+        Object.keys(vm.combatenteSimulador).forEach(percorreCombatentes);
+        
+        vm.atividadeTurno.forEach(percorreAtividades);
+    }
+
+    function percorreAtividades(atividade){
+        texto = vm.combatenteSimulador[atividade.chaveCombatente].nome;
+
+        switch(atividade.tipo){
+            case 0: texto += " iniciou"
+                break;
+            
+            case 1: texto += " está no meio de"
+                break;
+            
+            case 2: texto += " finalizou"
+                break;
+        }
+
+        texto += " " + vm.acoes[atividade.chaveAcao].nome + "(" + atividade.inicializacaoAtividade.toString() + ").\n";
+
+        vm.logCombate += texto;
+
+        console.log(atividade);
+    }
+
+    function percorreCombatentes(key){
+        acao = vm.acoes[vm.combatenteSimulador[key].acao];
+
+        novaAtividade(key, 
+            0, 
+            substituiInitECalculaAcao(
+                acao.inicio, 
+                vm.combatenteSimulador[key].inicializacao),
+            vm.combatenteSimulador[key].acao
+        );
+
+        novaAtividade(key, 
+            1, 
+            substituiInitECalculaAcao(
+                acao.meio, 
+                vm.combatenteSimulador[key].inicializacao),
+            vm.combatenteSimulador[key].acao
+        );
+
+        novaAtividade(key, 
+            2, 
+            vm.combatenteSimulador[key].inicializacao,
+            vm.combatenteSimulador[key].acao
+        );
+        
+    }
+
+    function novaAtividade(chaveCombatente, tipo, inicializacaoAtividade, chaveAcao){
+        atividade = {
+            chaveCombatente: chaveCombatente,
+            tipo: tipo,
+            inicializacaoAtividade: inicializacaoAtividade,
+            chaveAcao: chaveAcao
+        }
+
+        vm.atividadeTurno.push(atividade);
+    }
+
+    function substituiInitECalculaAcao(expressao, valor){
+        while(expressao.indexOf("init") > -1){
+            expressao = expressao.replace("init", valor.toString());
+        }            
+        
+        return Math.floor(eval(expressao));
+    }    
 
 }
