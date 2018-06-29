63131209747a8e804b6fde80561ac19fa6c7cac0
Guilherme Lacerda <gui.lls@hotmail.com>
2018-06-29 17:08:41 -0300

    Conversão parcial da view para classes.

--- site/apps/sistema/views.py
+++ site/apps/sistema/views.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.urls import reverse
-from distorcao.classviews import CustomCreateView
-from django.views.generic import CreateView
+from distorcao.classviews import CustomCreateView, CustomUpdateView, CustomDeleteView
 from apps.sistema.forms import SistemaForm
 from apps.sistema.models import *
-from distorcao.views import get_form_variables
 from distorcao.viewhelper import update_context
 from django.http import JsonResponse, HttpResponse
 from distorcao.json_complex_encoder import *
@@ -21,70 +18,38 @@ class SistemaCreate(CustomCreateView):
 
     def get_context_data(self, **kwargs):
         context_data = super(SistemaCreate, self).get_context_data(**kwargs)
-        context_data = update_context(context_data, 'Cadastro de Sistema', self.request.path)
+        context_data = update_context(context_data, 'Cadastro de Sistema')
+        return context_data
+
+class SistemaUpdate(CustomUpdateView):
+    template_name = 'sistema/fields.html'
+    form_class = SistemaForm
+    model = Sistema
+
+    def get_success_url(self):
+        return reverse('sistema_consulta')
+
+    def get_context_data(self, **kwargs):
+        context_data = super(SistemaUpdate, self).get_context_data(**kwargs)
+        context_data = update_context(context_data, 'Alteração de Sistema')
+        return context_data
+
+class SistemaDelete(CustomDeleteView):
+    template_name = 'sistema/delete.html'
+    form_class = SistemaForm
+    model = Sistema
+
+    def get_success_url(self):
+        return reverse('sistema_consulta')
+
+    def get_context_data(self, **kwargs):
+        context_data = super(SistemaDelete, self).get_context_data(**kwargs)
+        context_data = update_context(context_data, 'Exclusão de Sistema')
         return context_data
 
 def list(request):
     sistemas = Sistema.objects.all()
     return render(request, 'sistema/list.html', {'sistemas' : sistemas})
-
-def create(request):
-
-
-    if request.method == 'POST':
-        form = SistemaForm(request.POST)
-
-        if form.is_valid():        
-            form.save()
-
-            return redirect('sistema_consulta')
-        else:
-            form_variables = get_form_variables('Cadastro de Sistema', request.path, form)
-
-            return render(request, template_name, form_variables)
-    else:
-        form = SistemaForm()
-
-        form_variables = get_form_variables('Cadastro de Sistema', request.path, form)                
-
-        return render(request, template_name, form_variables)
-
-def update(request, sistema_id):
-    template_name = 'sistema/fields.html'
-    sistema = Sistema.objects.get(id=sistema_id)
-
-    if request.method == 'POST':        
-        form = SistemaForm(request.POST, instance=sistema)
-
-        if form.is_valid():
-            form.save()
-
-            return redirect('sistema_consulta')
-
-        else:
-            form_variables = get_form_variables('Alteração de Sistema', request.path, form)
-        
-            return render(request, template_name, form_variables)
-    else:    
-        form = SistemaForm(instance=sistema)
-
-        form_variables = get_form_variables('Alteração de Sistema', request.path, form=form)
-
-        return render(request, template_name, form_variables)
-
-def delete(request, sistema_id):
-    template_name = 'sistema/delete.html'
-
-    if request.method == 'POST':
-        sistema = Sistema.objects.get(id=sistema_id)        
-
-        sistema.delete()
-
-        return redirect('sistema_consulta')
-    else:
-        sistema = Sistema.objects.get(id=sistema_id)
-
-        return render(request, template_name, {'sistema': sistema})
 
 def get_lista_sistemas():
     lista_sistema = Sistema.objects.all()
--- site/distorcao/classviews.py
+++ site/distorcao/classviews.py
@@ -1,7 +1,17 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.utils.translation import activate
 
 class CustomCreateView(CreateView):
     def __init__(self):
         super(CustomCreateView, self).__init__()
+        activate('pt_BR')
+
+class CustomUpdateView(UpdateView):
+    def __init__(self):
+        super(CustomUpdateView, self).__init__()
+        activate('pt_BR')
+
+class CustomDeleteView(DeleteView):
+    def __init__(self):
+        super(CustomDeleteView, self).__init__()
         activate('pt_BR')
